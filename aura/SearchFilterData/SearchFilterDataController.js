43469d7bd57bb0f8469813aef1bc387a99ddd0a6
Chintan Shah <salesforce@springsoa.com>
2019-09-02 20:49:53 +0000

    CI deployment from job `Salesforce SpringSOA`

--- aura/SearchFilterData/SearchFilterDataController.js
+++ aura/SearchFilterData/SearchFilterDataController.js
@@ -1,15 +1,24 @@
 ({
     handleClick : function(component, event, helper) {
 
-        var accObj = component.get("v.selectedLookUpRecord");
-        var accountName = component.get("v.SearchKeyWord");
-        console.log(accObj.Id);
-        console.log(accountName);
-        var action = component.get('c.fetchOpportunity');
+        var accObj = component.get("v.selectedLookUpRecordAcc");
+        var accountName = component.get("v.SearchKeyWordAcc");
+        var conObj = component.get("v.selectedLookUpRecordCon");
+        var contactName = component.get("v.SearchKeyWordCon");
+
+        var action = component.get('c.fetchDataForFilters');
+        action.setParams({
+            'accountId': accObj.Id!= undefined? accObj.Id : '',
+            'accountStr' : accountName,
+            'contactId': conObj.Id!= undefined? conObj.Id : '',
+            'contactStr' : contactName
+        });
         action.setCallback(this, function(response) {
             //store state of response
             var state = response.getState();
             if (state === "SUCCESS") {
+                debugger;
+                console.log(JSON.parse(JSON.stringify(response.getReturnValue())));
                 component.set('v.lstOpp', response.getReturnValue());
                 component.set('v.dataAvailable',true);
             }
--- aura/dataTableDemo/dataTableDemoController.js
+++ aura/dataTableDemo/dataTableDemoController.js
@@ -5,12 +5,20 @@
     
     doInit : function(component,event,helper){
             setTimeout(function(){
+                var tableData = component.get('v.lstOpp');
+                console.log(JSON.parse(JSON.stringify(tableData)));
                 $('#tableId thead tr:eq(1) th').each( function () {
                     var title = $('#example thead tr:eq(0) th').eq( $(this).index() ).text();
                     $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
                 } );
                 var table = $('#tableId').DataTable({
-                    orderCellsTop: true
+                    orderCellsTop: true,
+                    data: tableData,
+                    "columns": [
+                        { "data": "accName" },
+                        { "data": "con.LastName" , "defaultContent": "<i>Not set</i>"},
+                        { "data": "opp.Name", "defaultContent": "<i>Not set</i>"}
+                    ]
                 });
                 table.columns().every( function (index) {
                     $('#tableId thead tr:eq(1) th:eq(' + index + ') input').on('keyup change', function () {
