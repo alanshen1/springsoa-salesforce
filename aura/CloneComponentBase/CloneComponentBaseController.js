c603c58b5f5d3f868144fd7e62c802e1678d07aa
Chintan Shah <salesforce@springsoa.com>
2019-09-20 04:49:55 +0000

    CI deployment from job `Salesforce SpringSOA`

--- aura/CloneComponentBase/CloneComponentBaseController.js
+++ aura/CloneComponentBase/CloneComponentBaseController.js
@@ -7,12 +7,28 @@
         var action = component.get('c.getRecord');
         var recordId = component.get('v.recordId');
         var fieldSetName = component.get('v.fieldSetName');
+        var sObjectApiName = component.get('v.sObjectApiName');
         action.setParams({recordId: recordId,fieldSetName:fieldSetName,sObjectApiName:sObjectApiName});
         action.setCallback(this, function (response) {
             if(response.getState() === 'SUCCESS'){
                 var defaultFieldValues = response.getReturnValue();
                 helper.createRecord(component, defaultFieldValues);
                 helper.closeQuickActionModal(component);
+            }else if (response.getState() ==="ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        var toastEvent = $A.get("e.force:showToast");
+                        toastEvent.setParams({
+                            "title": "Error",
+                            "message": errors[0].message
+                        });
+                        toastEvent.fire();
+                        helper.closeQuickActionModal(component);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
             }
         });
         $A.enqueueAction(action);
