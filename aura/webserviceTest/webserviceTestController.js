4d38d9cdcc248092f1a1634a19137713989c9791
Chintan Shah <salesforce@springsoa.com>
2019-09-30 04:49:51 +0000

    CI deployment from job `Salesforce SpringSOA`

--- aura/webserviceTest/webserviceTestController.js
+++ aura/webserviceTest/webserviceTestController.js
@@ -4,16 +4,20 @@
 
 ({
     doInit: function (c, e, h) {
+    },
+    postData: function (c, e, h) {
+        c.set('v.isProcessing', true);
         var xhr = new XMLHttpRequest();
         var url = 'https://basic-authentication-ws.herokuapp.com/echo';
         xhr.open('POST', url, true);
         xhr.onload = function () {
             if (this.status === 200) {
-                console.log(this.response);
+                c.set('v.isProcessing', false);
+                c.set('v.response', this.response);
             }
         };
 
         xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        xhr.send(JSON.stringify({message: "Test"}));
+        xhr.send(JSON.stringify({message: c.get('v.message')}));
     }
 });
