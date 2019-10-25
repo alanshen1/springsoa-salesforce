611391e8bb136610b1851f2b496605b488900c6b
Chintan Shah <salesforce@springsoa.com>
2019-10-25 16:49:47 +0000

    CI deployment from job `springsoa`

---
+++ aura/HighlightDOM/HighlightDOMController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by spring4 on 25-10-2019.
+ */
+
+({
+    highlight : function (cmp,eve,helper) {
+
+        $( "#toggle" ).effect( "highlight", {color:"#669966"}, 3000 );
+
+    }
+});
---
+++ aura/HighlightDOMApp/HighlightDOMApp.app
@@ -0,0 +1,7 @@
+<!--
+ - Created by spring4 on 25-10-2019.
+ -->
+
+<aura:application description="HighlightDOMApp" extends="force:slds">
+    <c:HighlightDOM />
+</aura:application>
